761189a376d8cb12653e21e05e2057aa81b76e8d
alfiiatech <alfiya.sr@techversantinfotech.com>
2023-05-08 17:26:21 +0530

    cropped image in localsorage

--- src/app/cropper/cropper.component.ts
+++ src/app/cropper/cropper.component.ts
@@ -1,25 +1,91 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnInit } from '@angular/core';
 import Cropper from 'cropperjs';
+import { LocalService } from '../local.service';
 
 @Component({
   selector: 'app-cropper',
   templateUrl: './cropper.component.html',
   styleUrls: ['./cropper.component.css']
 })
-
-export class CropperComponent {
+export class CropperComponent implements AfterViewInit, OnInit {
   @ViewChild('image', { read: ElementRef }) image!: ElementRef;
 
   private cropper!: Cropper;
+  private croppedImages: { top: number, left: number, width: number, height: number }[] = [];
+
+  constructor(public localService: LocalService) { }
+
+  ngOnInit() {
+    const storedImages = this.localService.getItem('croppedImages');
+    if (storedImages) {
+      this.croppedImages = JSON.parse(storedImages);
+    }
+  }
 
   ngAfterViewInit() {
     this.cropper = new Cropper(this.image.nativeElement, {
-      aspectRatio: 1,
+      aspectRatio: 1 / 2,
       viewMode: 3,
-      autoCropArea: 1,
-      dragMode: 'move'
-    });
+      autoCropArea: 0.65,
+      restore: true,
+      dragMode: 'crop',
+      cropBoxMovable: true,
+      guides: false,
+      center: false,
+      cropBoxResizable: true,
+      toggleDragModeOnDblclick: false,
+      ready: (event: any) => {
+        setTimeout(() => {
+          this.restoreCroppedElements();
+          this.saveCroppedElement();
+        },);
 
+      }
+    });
+  }
+
+  saveCroppedElement() {
+    const cropBoxData = this.cropper.getCropBoxData();
+    const croppedElement = {
+      top: cropBoxData.top,
+      left: cropBoxData.left,
+      width: cropBoxData.width,
+      height: cropBoxData.height,
+      color: 'gray'
+    };
+    this.croppedImages.push(croppedElement);
+    this.localService.setItem('croppedImages', JSON.stringify(this.croppedImages));
+  }
+
+  restoreCroppedElements() {
+    const storedCroppedElements = this.localService.getItem('croppedImages');
+    if (storedCroppedElements) {
+      this.croppedImages = JSON.parse(storedCroppedElements);
+
+      const divPosition = document.getElementById('imageDiv');
+      const imageTop: any = divPosition?.offsetTop;
+      const imageLeft: any = divPosition?.offsetLeft;
+
+      this.croppedImages.forEach((croppedElement) => {
+        const newCroppedElement = document.createElement('div');
+        newCroppedElement.style.position = 'absolute';
+        newCroppedElement.style.top =  croppedElement.top + 'px';
+        newCroppedElement.style.left = croppedElement.left + 'px';
+        newCroppedElement.style.width = croppedElement.width + 'px';
+        newCroppedElement.style.height = croppedElement.height + 'px';
+        newCroppedElement.style.backgroundColor = 'gray';
+
+        newCroppedElement.classList.add('cropped-card');
+
+        const container = this.image.nativeElement.parentElement;
+        container.appendChild(newCroppedElement);
+      });
+
+      const lastCroppedElement = this.croppedImages[this.croppedImages.length - 1];
+      if (lastCroppedElement) {
+        this.cropper.setCropBoxData(lastCroppedElement);
+      }
+    }
   }
 
 
@@ -27,62 +93,37 @@ export class CropperComponent {
   crop() {
     const canvas = this.cropper.getCroppedCanvas();
     const dataURL = canvas.toDataURL();
-    canvas.style.display = "none";
-
-    const file = this.dataURLtoFile(dataURL, 'croppedImage.png');
-    console.log(file);
-    // localStorage.setItem("image ", 'croppedImage.png');
-    // console.log(localStorage)
+    canvas.style.display = 'none';
 
     const cropBoxData = this.cropper.getCropBoxData();
-    console.log("cropdata", cropBoxData);
-    this.cropper.setCropBoxData({
-      left: cropBoxData.left,
-      top: cropBoxData.top,
-      width: cropBoxData.width,
-      height: cropBoxData.height
-    });
-
-
     const divPosition = document.getElementById('imageDiv');
     const imageTop: any = divPosition?.offsetTop;
     const imageLeft: any = divPosition?.offsetLeft;
 
-    console.log("top", imageTop);
-    console.log("left", imageLeft);
+    const croppedElement = {
+      top: imageTop + cropBoxData.top,
+      left: imageLeft + cropBoxData.left,
+      width: cropBoxData.width,
+      height: cropBoxData.height,
+    };
 
-
-    const croppedElement = document.createElement('div');
-    croppedElement.style.position = 'absolute';
-    croppedElement.style.top = (imageTop + this.cropper.getCropBoxData().top) + 'px';
-    croppedElement.style.left = (imageLeft + this.cropper.getCropBoxData().left) + 'px';
-    croppedElement.style.width = this.cropper.getCropBoxData().width + 'px';
-    croppedElement.style.height = this.cropper.getCropBoxData().height + 'px';
-    croppedElement.style.backgroundColor = 'gray';
-    croppedElement.classList.add('cropped-card');
+    const newCroppedElement = document.createElement('div');
+    newCroppedElement.style.position = 'absolute';
+    newCroppedElement.style.top = croppedElement.top + 'px';
+    newCroppedElement.style.left = croppedElement.left + 'px';
+    newCroppedElement.style.width = croppedElement.width + 'px';
+    newCroppedElement.style.height = croppedElement.height + 'px';
+    newCroppedElement.style.backgroundColor = 'gray';
+    newCroppedElement.classList.add('cropped-card');
 
     const container = this.image.nativeElement.parentElement;
-    container.appendChild(croppedElement);
+    container.appendChild(newCroppedElement);
 
+    this.croppedImages.push(croppedElement);
+    this.localService.setItem('croppedImages', JSON.stringify(this.croppedImages));
 
+    this.cropper.reset();
   }
 
-
-  private dataURLtoFile(dataURL: string, filename: string): File {
-    const arr = dataURL.split(',');
-    const mime = arr[0].match(/:(.*?);/)![1];
-    const bstr = atob(arr[1]);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-
-    while (n--) {
-      u8arr[n] = bstr.charCodeAt(n);
-    }
-
-    return new File([u8arr], filename, { type: mime });
-  }
-}
-
-function useRef<T>(arg0: null) {
-  throw new Error('Function not implemented.');
+  
 }
---
+++ src/app/local.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalService {
+
+  getItem(key: string): any {
+    return localStorage.getItem(key);
+  }
+
+  setItem(key: string, value: any): void {
+    localStorage.setItem(key, value);
+    // localStorage.clear();
+
+  }
+
+  public getData(key: string) {
+    return localStorage.getItem(key)
+  }
+  public removeData(key: string) {
+    localStorage.removeItem(key);
+  }
+
+  public clearData() {
+    localStorage.clear();
+  }
+
+  constructor() { }
+}
